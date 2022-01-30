403b263294a506039e96f5f84091640066f4ca8c
christian esgrina <hansesgrina.06@gmail.com>
2022-01-30 20:29:03 +0800

    first commit

---
+++ heart.js
@@ -0,0 +1,34 @@
+
+    var i = 0;
+    var txt = "Hi, I want u to know that you're very special to me & i really like you.\n HAPPY HEARTS DAY!!!"; /* The text */
+    var speed = 55; /* The speed/duration of the effect in milliseconds */
+    
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+        document.getElementById("subtitle").style.display = "none";
+        document.getElementById("chng").style.color = "red";
+        const container = document.querySelector('.container');
+
+
+        const creat =document.createElement ('div');
+        creat.classList.add('hearts');
+        creat.innerHTML = ' 💗️';
+    
+        creat.style.left = Math.random() * 100 + 'vw';
+    
+        creat.style.animationDuration = Math.random()* 3 + 2 + 'vw';
+        container.appendChild(creat);
+         setTimeout(() => {
+              creat.remove ();
+         }, 2000 );
+     
+        }
+        setInterval(hearts,  150);
+      }
+      
+   
+ 
+  
